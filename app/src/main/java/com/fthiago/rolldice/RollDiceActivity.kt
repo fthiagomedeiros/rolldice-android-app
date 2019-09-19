4241567e55661a870e6c43963190b5ddb0a3cf93
thiago <fthiagomedeiros@gmail.com>
2019-09-19 13:59:09 -0300

    Roll Dice MVVM

--- app/src/main/java/com/fthiago/rolldice/RollDiceActivity.kt
+++ app/src/main/java/com/fthiago/rolldice/RollDiceActivity.kt
@@ -1,12 +1,13 @@
 package com.fthiago.rolldice
 
-import android.database.DatabaseUtils
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.fthiago.rolldice.model.Dice
 
 class RollDiceActivity : AppCompatActivity() {
 
@@ -24,8 +25,7 @@ class RollDiceActivity : AppCompatActivity() {
     }
 
     private fun observeRollDice() {
-        viewModel.getDiceValue().observe(this, androidx.lifecycle.Observer {
-            Toast.makeText(this, "Funcionou $it", Toast.LENGTH_LONG).show()
+        viewModel.getDiceValue().observe(this, Observer {
             drawDice(it)
         })
     }
@@ -34,14 +34,15 @@ class RollDiceActivity : AppCompatActivity() {
         viewModel.rollDice()
     }
 
-    private fun drawDice(diceValue: Int) {
-        val drawableResource = when(diceValue) {
+    private fun drawDice(dice: Dice) {
+        val drawableResource = when(dice.diceValue) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
         }
 
         diceImage.setImageResource(drawableResource)
--- app/src/main/java/com/fthiago/rolldice/RollDiceViewModel.kt
+++ app/src/main/java/com/fthiago/rolldice/RollDiceViewModel.kt
@@ -3,18 +3,19 @@ package com.fthiago.rolldice
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.fthiago.rolldice.model.Dice
 import java.util.*
 
 class RollDiceViewModel : ViewModel() {
 
-    val diceValue: MutableLiveData<Int> = MutableLiveData()
+    val diceValue: MutableLiveData<Dice> = MutableLiveData()
 
     fun rollDice() {
         val diceValue = Random().nextInt(6) + 1
-        this.diceValue.value = diceValue
+        this.diceValue.value = Dice(diceValue)
     }
 
-    fun getDiceValue(): LiveData<Int> {
+    fun getDiceValue(): LiveData<Dice> {
         return diceValue
     }
 
